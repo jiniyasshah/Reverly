58612f52ce0d6ed966db861f1ed2d765974762cb
Sanket Shah <sanketshahts@gmail.com>
2023-12-31 14:40:35 +0545

    Initial commit

---
+++ components/shared/Footer.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import Image from "next/image";
+const Footer = () => {
+  return (
+    <footer className="border-t">
+      <div className="flex-center wrapper flex-between flex flex-col gap-4 p-5 text-center sm:flex-row">
+        <Link href="/">
+          <Image
+            src="assets/images/logo.svg"
+            alt="logo"
+            height={38}
+            width={128}
+          />
+        </Link>
+        <p>2023 Reverly. All Rights reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ components/shared/MobileNav.tsx
@@ -0,0 +1,40 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { Separator } from "@/components/ui/separator";
+import Image from "next/image";
+import NavItems from "./NavItems";
+const MobileNav = () => {
+  return (
+    <nav className="md:hidden">
+      <Sheet>
+        <SheetTrigger className="align-middle">
+          <Image
+            src="/assets/icons/menu.svg"
+            height={24}
+            width={24}
+            alt="menu"
+            className="cursor-pointer"
+          />
+        </SheetTrigger>
+        <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
+          <Image
+            src="/assets/images/logo.svg"
+            alt="logo"
+            height={38}
+            width={128}
+          />
+          <Separator className="border border-gray-50" />
+          <NavItems />
+        </SheetContent>
+      </Sheet>
+    </nav>
+  );
+};
+
+export default MobileNav;
